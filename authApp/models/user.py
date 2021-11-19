1fdc15fb33bcced0ca95c72d018e666b22880036
Ivan Yared Lombana Lozano <ilombana@unal.edu.co>
2021-11-18 20:38:32 -0500

    Fixing migrations

--- authApp/models/user.py
+++ authApp/models/user.py
@@ -31,9 +31,9 @@ class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField('Username', max_length = 15, unique=True)
     password = models.CharField('Password', max_length = 256)
     name = models.CharField('Name', max_length = 30)
-    lastName = models.CharField('LastName', default="LastName", max_length = 30)
+    lastName = models.CharField('LastName', max_length = 30)
     email = models.EmailField('Email', max_length = 100)
-    address = models.CharField('Address', default="Address", max_length= 200)
+    address = models.CharField('Address', max_length= 200)
 
     def save(self, **kwargs):
         some_salt = 'mMUj0DrIK6vgtdIYepkIxN' 
--- authApp/serializers/userSerializer.py
+++ authApp/serializers/userSerializer.py
@@ -4,4 +4,4 @@ from authApp.models.user import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'password', 'name', 'lastname', 'email', 'address']
+        fields = ['id', 'username', 'password', 'name', 'email']
