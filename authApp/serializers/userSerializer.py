5f02d587d6ff7e972b64d47a7891dc2f8a1c3d38
Ivan Yared Lombana Lozano <ilombana@unal.edu.co>
2021-11-22 15:30:57 -0500

    Bug lastName, address fixed

--- authApp/serializers/userSerializer.py
+++ authApp/serializers/userSerializer.py
@@ -4,4 +4,4 @@ from authApp.models.user import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'password', 'name', 'email']
+        fields = ['id', 'username', 'password', 'name', 'lastName', 'email', 'address']
